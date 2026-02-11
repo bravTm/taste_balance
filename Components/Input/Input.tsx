309e379f9dbbe33af53d86fdf528d537af986a76
Eugene <98524775+bravTm@users.noreply.github.com>
2026-02-11 17:32:47 +0300

    Update Input.tsx

--- Components/Input/Input.tsx
+++ Components/Input/Input.tsx
@@ -17,8 +17,8 @@ const Input:FC<IInput> = ({ text, handleChange }) => {
             placeholder="Имя"
             required
 
-            className='w-[80%] bg-[#F0F1F1] border-[1px] h-[9%] rounded-lg p-[2%]
-            hover:bg-[#e3e8e8] transition-all'
+            className='w-[80%] bg-[#EEE8BF] border-[1px] h-[9%] rounded-lg p-[2%]
+            hover:bg-[#ded8b1] transition-all'
         />
     )
 }
