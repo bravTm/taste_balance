5058568649accdc75643022199d4d6137f85112d
Eugene <98524775+bravTm@users.noreply.github.com>
2026-02-11 17:35:33 +0300

    Update Button.tsx

--- Components/Button/Button.tsx
+++ Components/Button/Button.tsx
@@ -8,8 +8,8 @@ interface IButton {
 const Button:FC<IButton> = ({ title, onClick }) => {
 
     return (
-        <div className='w-[50%] flex justify-center items-center bg-[#4dc48c] cursor-pointer p-[2%] rounded-sm
-        mt-[5%] mb-[4%] hover:bg-[#73e069] transition-all'
+        <div className='w-[50%] flex justify-center items-center bg-[#79793F] cursor-pointer p-[2%] rounded-sm
+        mt-[5%] mb-[4%] hover:bg-[#78864B] transition-all text-white'
             onClick={onClick}
         >
             <p>
