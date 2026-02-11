f87cb56de79aa0035e035cc3e853de3a35f4d796
Eugene <98524775+bravTm@users.noreply.github.com>
2026-02-11 17:27:26 +0300

    Update Dish.tsx

--- Components/Dish/Dish.tsx
+++ Components/Dish/Dish.tsx
@@ -8,12 +8,12 @@ interface IDishComponent {
 const Dish:FC<IDishComponent> = ({ text, title }) => {
 
     return text ? (
-        <div className='w-full flex justify-between text-lg mt-[0.5%] mb-[0.5%]'>
-            <p className='w-[25%] font-bold bg-green-400 flex justify-center items-center p-[1%] rounded-l-sm'>
+        <div className='w-full flex justify-between text-lg mt-[0.5%] mb-[0.5%] '>
+            <p className='w-[25%] font-bold bg-[#BDB67D] flex justify-center items-center p-[1%] rounded-l-sm hover:bg-[#d1ca8c] transition-all'>
                 {title}
             </p>
  
-            <p className='w-[75%] bg-red-300 flex justify-center items-center p-[1%] rounded-r-sm'>
+            <p className='w-[75%] bg-[#EEE8BF] border-[1px] flex justify-center items-center p-[1%] rounded-r-sm hover:bg-[#d9d3ad] transition-all'>
                 {text}
             </p>
         </div>
